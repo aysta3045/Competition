627314ab6356c9c61d11d3d4dc572c31eb7b3614
aysta3045 <2132895295@qq.com>
2026-01-13 14:26:55 +0800

    update

---
+++ src/client/java/aysta3045/CompetitionClient.java
@@ -0,0 +1,10 @@
+package aysta3045;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CompetitionClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
