4d6003a64cf6b5ef1309704faac2e62d0ab05c03
aysta3045 <2132895295@qq.com>
2026-01-13 09:37:03 +0800

    Initial commit

---
+++ src/main/java/aysta3045/CompetitionDataGenerator.java
@@ -0,0 +1,11 @@
+package aysta3045;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CompetitionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
